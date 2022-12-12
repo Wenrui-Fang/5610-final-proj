eb213021383554e85f92b6232dfe46695e853f9d
kimrine <347642700@qq.com>
2022-12-12 14:26:37 -0800

    add detailpage v2

--- src/DetailScreen/index.js
+++ src/DetailScreen/index.js
@@ -11,128 +11,36 @@ const DetailComponent = () => {
 
     const {businessId} = useParams();
     const [business, setBusiness] = useState({
-                                                 "id": "6I28wDuMBR5WLMqfKxaoeg",
-                                                 "alias": "pike-place-chowder-seattle",
-                                                 "name": "Pike Place Chowder",
-                                                 "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/ZyQjV-wJQ2GHyX7l3jfbyg/o.jpg",
-                                                 "is_claimed": false,
-                                                 "is_closed": false,
-                                                 "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=moTah-sZXPH7mohl5JGWhw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=moTah-sZXPH7mohl5JGWhw",
-                                                 "phone": "+12062672537",
-                                                 "display_phone": "(206) 267-2537",
-                                                 "review_count": 8528,
-                                                 "categories": [
-                                                     {
-                                                         "alias": "seafood",
-                                                         "title": "Seafood"
-                                                     },
-                                                     {
-                                                         "alias": "soup",
-                                                         "title": "Soup"
-                                                     }
-                                                 ],
-                                                 "rating": 4.5,
-                                                 "location": {
-                                                     "address1": "1530 Post Aly",
-                                                     "address2": "Ste 11",
-                                                     "address3": "",
-                                                     "city": "Seattle",
-                                                     "zip_code": "98101",
-                                                     "country": "US",
-                                                     "state": "WA",
-                                                     "display_address": [
-                                                         "1530 Post Aly",
-                                                         "Ste 11",
-                                                         "Seattle, WA 98101"
-                                                     ],
-                                                     "cross_streets": "Pike Pl & Pine St"
-                                                 },
-                                                 "coordinates": {
-                                                     "latitude": 47.60939,
-                                                     "longitude": -122.34112
-                                                 },
-                                                 "photos": [
-                                                     "https://s3-media1.fl.yelpcdn.com/bphoto/ZyQjV-wJQ2GHyX7l3jfbyg/o.jpg",
-                                                     "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
-                                                     "https://s3-media1.fl.yelpcdn.com/bphoto/9SH1ddW7ZpOM_dZupD7VlQ/o.jpg"
-                                                 ],
-                                                 "price": "$$",
-                                                 "hours": [
-                                                     {
-                                                         "open": [
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 0
-                                                             },
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 1
-                                                             },
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 2
-                                                             },
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 3
-                                                             },
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 4
-                                                             },
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 5
-                                                             },
-                                                             {
-                                                                 "is_overnight": false,
-                                                                 "start": "1100",
-                                                                 "end": "1700",
-                                                                 "day": 6
-                                                             }
-                                                         ],
-                                                         "hours_type": "REGULAR",
-                                                         "is_open_now": false
-                                                     }
-                                                 ],
-                                                 "transactions": [
-                                                     "delivery",
-                                                     "pickup"
-                                                 ]
                                              });
     useEffect(() => {
-       // try{
-       //     console.log(businessId);
-       //     const getBusiness = async() => await yelpService.findBusinessById(businessId)
-       //         .then(bus => setBusiness(bus));
-       //     let bus = getBusiness();
-       // } catch (e){
-       //
-       // }
+       try{
+           const getBusiness = async() => await yelpService.findBusinessById(businessId)
+               .then(bus => setBusiness(bus));
+           let bus = getBusiness();
+       } catch (e){
+
+       }
     });
 
 
-    let startStr = business.hours[0].open[3].start.toString();
-    let startTime = startStr.substring(0,2) + ":" + startStr.substring(2);
-    let endStr = business.hours[0].open[3].end.toString();
-    let endTime = endStr.substring(0,2) + ":" + endStr.substring(2);
+    let startStr = business.hours?business.hours[0].open[3].start.toString():undefined;
+    let startTime = startStr===undefined?undefined:startStr.substring(0,2) + ":" + startStr.substring(2);
+    let endStr = business.hours?business.hours[0].open[3].end.toString():undefined;
+    let endTime = endStr===undefined?undefined:endStr.substring(0,2) + ":" + endStr.substring(2);
+
+    const getTime = (str) => {
+        return str.substring(0,2) + ":" + str.substring(2);
+    }
+
     return (
         <>
             <h1>Detail Screen</h1>
             <div className="photoHeader mb-2 position-relative">
                 <div className="flex-container">
+                    {
+                        business.photos && business.photos.map(photo => {return(<img className="headerPicture "
+                                                                                     src={`${photo}`} />)})
+                    }
                     {
                         business.photos && business.photos.map(photo => {return(<img className="headerPicture "
                                                                                      src={`${photo}`} />)})
@@ -159,7 +67,10 @@ const DetailComponent = () => {
 
                         {business.is_closed&& <span className="text-red">Closed </span>}
                         {!business.is_closed&& <span className="text-red">Opening </span>}
-                        <b><span className="text-white">{startTime} AM - {endTime} PM</span></b>
+                        {
+                            startTime!==undefined&&endTime!==undefined&&<b><span className="text-white">{startTime} AM - {endTime} PM</span></b>
+                        }
+
                     </div>
                 </div>
             </div>
@@ -181,13 +92,13 @@ const DetailComponent = () => {
                         <div>
                             Float left
                         </div>
-                        <div className="mt-2 col-2 col-md-2 col-lg-1 col-xl-2 float-end">
+                        <div className="float-end">
                             <span className="subtitle">Hours</span>
                             <table className="hourTable">
                                 <tbody>
                                 <tr>
                                     <td className="td-width">Mon</td>
-                                    <td>Closed</td>
+                                    <td className="">{getTime(business.hours[0].open[0].start.toString())} AM - {getTime(business.hours[0].open[0].end.toString())} PM</td>
                                 </tr>
                                 <tr>
                                     <td className="td-width">Tue</td>
