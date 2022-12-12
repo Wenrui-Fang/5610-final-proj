58b9487f26bfce5599a25454af3abad9eae15874
fangwenrui <fangwenrui1997@outlook.com>
2022-12-11 22:30:18 -0800

    update review screen

--- src/HomePostsList/PostItem.js
+++ src/HomePostsList/PostItem.js
@@ -7,7 +7,7 @@ const PostItem = (props) => {
     let loggedIn = props.loggedIn
     return (
         <li className="d-inline-block">
-            <Link to={loggedIn ? `/review/${business.id}`: "/login"} className="text-decoration-none text-black">
+            <Link to={loggedIn ? `/review/${business.name}/${business.id}`: "/login"} className="text-decoration-none text-black">
                 <div className="row ms-5 me-5 ps-3 mb-4">
                     <div className="col-5">
                         <img width={150} height={150} className="rounded-3" alt="business"
